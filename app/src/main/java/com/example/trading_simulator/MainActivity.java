2816e0ebfc240f25561599dda9c842c9192e77a4
Steven Liu <xiaoxiang.liu48@gmail.com>
2020-05-18 20:57:59 +0800

    Added listview

--- app/src/main/java/com/example/trading_simulator/MainActivity.java
+++ app/src/main/java/com/example/trading_simulator/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.trading_simulator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,12 +15,51 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+    String[] stockName = {"Apple", "Banana", "Litchi", "Mango", "PineApple","Apple", "Banana", "Litchi", "Mango", "PineApple","Apple", "Banana", "Litchi", "Mango", "PineApple"};//fruit names array
+    int[] price ={111, 222, 333, 444, 555, 111, 222, 333, 444, 555, 111, 222, 333, 444, 555};
+    ListView simpleListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        simpleListView=(ListView)findViewById(R.id.simpleListView);
+
+        ArrayList<HashMap<String,String>> arrayList=new ArrayList<>();
+        for (int i=0;i<stockName.length;i++)
+        {
+            HashMap<String,String> hashMap=new HashMap<>();//create a hashmap to store the data in key value pair
+            hashMap.put("name",stockName[i]);
+            hashMap.put("image",price[i]+"");
+            arrayList.add(hashMap);//add the hashmap into arrayList
+        }
+        String[] from={"name","image"};//string array
+        int[] to={R.id.textView,R.id.textView2};//int array of views id's
+        SimpleAdapter simpleAdapter=new SimpleAdapter(this,arrayList,R.layout.list_view_items,from,to);//Create object and set the parameters for simpleAdapter
+        simpleListView.setAdapter(simpleAdapter);//sets the adapter for listView
+
+        //perform listView item click event
+        simpleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),stockName[i], Toast.LENGTH_LONG).show();//show the selected image in toast according to position
+            }
+        });
+
+
+
+
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
